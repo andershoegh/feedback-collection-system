84d2eecf13b16bd26610feafe87c5639941aec01
BAD MOTHERFUCKER <lassestausgaard@gmail.com>
2021-02-25 16:10:06 +0100

    Initial setup of page and functionality

---
+++ src/QuestionTypes/SliderQuestion.tsx
@@ -0,0 +1,87 @@
+import React, { useRef, useState } from 'react';
+
+export interface SliderQuestionProps {
+    currentStep: number;
+    renderOnStep: number;
+    question: string;
+    subText: string;
+    rangeMin: number;
+    rangeMax: number;
+    intervals?: number;
+    startValue: number;
+    minLabel?: string;
+    maxLabel?: string;
+}
+
+const SliderQuestion: React.FC<SliderQuestionProps> = (props) => {
+    const { currentStep, renderOnStep, question, subText, rangeMin, rangeMax, intervals, startValue, minLabel, maxLabel } = props;
+    const labelsRef = useRef<HTMLSpanElement>(null);
+    const [selectedValue, setSelectedValue] = useState<number>(startValue);
+    const highlightedClasses = ['font-semibold', 'text-2xl', 'text-indigo-500'];
+
+    const updateSelected = (value: number) => {
+        if (intervals && labelsRef.current) {
+            if (selectedValue !== (null || undefined)) {
+                labelsRef.current.children[selectedValue].classList.remove(...highlightedClasses);
+            }
+            labelsRef.current.children[value].classList.add(...highlightedClasses);
+        } else if (labelsRef.current) {
+            let scaleFactorMin = ((rangeMax - value) * 1.5) / (rangeMax - rangeMin) + 1;
+            let scaleFactorMax = ((value - rangeMin) * 1.5) / (rangeMax - rangeMin) + 1;
+
+            labelsRef.current.children[0].setAttribute('style', `transform: scale(${scaleFactorMin});`);
+            labelsRef.current.children[1].setAttribute('style', `transform: scale(${scaleFactorMax});`);
+        }
+        setSelectedValue(value);
+    };
+
+    const setIntervalLabels = () => {
+        if (intervals) {
+            let label = rangeMin;
+            const labels = [];
+
+            while (label <= rangeMax) {
+                let labelText = minLabel && label === rangeMin ? minLabel : maxLabel && label === rangeMax ? maxLabel : label;
+                let classes = label === selectedValue ? highlightedClasses.join(' ') : '';
+
+                labels.push(<span className={classes}>{labelText}</span>);
+                label++;
+            }
+            return labels;
+        } else {
+            let classes = 'inline-block ease-in-out transform scale-100 transition';
+            let labels = [<span className={classes}>{minLabel}</span>, <span className={classes}>{maxLabel}</span>];
+            return labels;
+        }
+    };
+
+    return (
+        <>
+            {currentStep !== renderOnStep ? null : (
+                <div className="w-4/5">
+                    <div className="text-3xl leading-10 font-medium">{question}</div>
+                    <div className="font-normal">{subText}</div>
+                    <div className="justify-center mt-40">
+                        <input
+                            type="range"
+                            min={rangeMin}
+                            max={rangeMax}
+                            value={selectedValue}
+                            step={intervals}
+                            onChange={(e) => updateSelected(parseInt(e.target.value))}
+                            className="block w-full mb-8"
+                        />
+                        <span ref={labelsRef} className="block flex justify-between font-normal text-lg text-blue-500">
+                            {setIntervalLabels()}
+                        </span>
+                    </div>
+                    <button className="border-4 border-transparent shadow-inactive focus:shadow-focused focus:outline-none focus:border-4 focus:border-blue-500 focus:border-opacity-100 py-6 px-32 text-3xl rounded-xl m-4">
+                        Next
+                    </button>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default SliderQuestion;
--- src/Questionnaire.tsx
+++ src/Questionnaire.tsx
@@ -1,29 +1,47 @@
-import React, { useState } from "react";
-import ButtonQuestion from "./QuestionTypes/ButtonQuestion";
-import RankedListQuestion from "./QuestionTypes/RankedListQuestion";
+import React, { useState } from 'react';
+import ButtonQuestion from './QuestionTypes/ButtonQuestion';
+import RankedListQuestion from './QuestionTypes/RankedListQuestion';
+import SliderQuestion from './QuestionTypes/SliderQuestion';
 
 export interface QuestionnaireProps {}
 
 const Questionnaire: React.SFC<QuestionnaireProps> = () => {
-  const [currentStep, setCurrentStep] = useState<number>(1);
+    const [currentStep, setCurrentStep] = useState<number>(1);
 
-  return (
-    <div className="h-screen w-2/3 flex justify-center items-center">
-      <button onClick={() => setCurrentStep(currentStep + 1)}>
-        Click to advance
-      </button>
-      <ButtonQuestion
-        currentStep={currentStep}
-        renderOnStep={1}
-        question={
-          "Are you satisfied with peoples ability to stay socially distanced throughout the store?"
-        }
-        firstButtonText={"Yes"}
-        secondButtonText={"No"}
-      />
-      <RankedListQuestion currentStep={currentStep} renderOnStep={2} />
-    </div>
-  );
+    return (
+        <div className="h-screen w-2/3 flex justify-center items-center">
+            <button onClick={() => setCurrentStep(currentStep + 1)}>Click to advance</button>
+            <SliderQuestion
+                currentStep={currentStep}
+                renderOnStep={1}
+                question={'How many people were with you while shopping today?'}
+                subText={'Swipe right or left to adjust the slider.'}
+                rangeMin={0}
+                rangeMax={4}
+                intervals={1}
+                startValue={2}
+            />
+            <SliderQuestion
+                currentStep={currentStep}
+                renderOnStep={2}
+                question={'How was your shopping trip today?'}
+                subText={'Swipe right or left to adjust the slider.'}
+                rangeMin={0}
+                rangeMax={100}
+                startValue={50}
+                minLabel={'😫'}
+                maxLabel={'😄'}
+            />
+            <ButtonQuestion
+                currentStep={currentStep}
+                renderOnStep={3}
+                question={'Are you satisfied with peoples ability to stay socially distanced throughout the store?'}
+                firstButtonText={'Yes'}
+                secondButtonText={'No'}
+            />
+            <RankedListQuestion currentStep={currentStep} renderOnStep={4} />
+        </div>
+    );
 };
 
 export default Questionnaire;
