5e0fd366568b8e0a92e57a8a4f2b88081a1cb279
Anders H <ahha15@student.aau.dk>
2021-03-03 11:50:46 +0100

    Added firestore connection

---
+++ src/Firebase/firebase.ts
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const config = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PID,
+  storageBucket: process.env.REACT_APP_SB,
+  messagingSenderId: process.env.REACT_APP_SID,
+  appId: process.env.REACT_APP_APPID,
+};
+
+firebase.initializeApp(config);
+
+export const useFirestore = () => {
+  const firestore = firebase.firestore();
+  return firestore;
+};
