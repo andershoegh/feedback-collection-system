c5abb3314a8c1553873cf74e7ec4fd836b5f6331
Anders H <ahha15@student.aau.dk>
2021-03-03 10:42:44 +0100

    fixes

--- src/QuestionTypes/TextQuestion.tsx
+++ src/QuestionTypes/TextQuestion.tsx
@@ -1,15 +1,15 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import BackButton from '../BackButton';
-import NextButton from '../NextButton';
-import './TextQuestion.css';
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import BackButton from "../BackButton";
+import NextButton from "../NextButton";
+import "./TextQuestion.css";
 
 export interface TextQuestionProps {
-    currentStep: number;
-    renderOnStep: number;
-    question: string;
-    subText: string;
-    handleChoice: (question: string, answer: string | number | string[]) => void;
-    goBackOneStep: () => void;
+  currentStep: number;
+  renderOnStep: number;
+  question: string;
+  subText: string;
+  handleChoice: (question: string, answer: string | number | string[]) => void;
+  goBackOneStep: () => void;
 }
 
 const TextQuestion: React.FC<TextQuestionProps> = (props) => {
@@ -28,108 +28,112 @@ const TextQuestion: React.FC<TextQuestionProps> = (props) => {
   const [redoAnswer, setRedoAnswer] = useState(false);
   const dotsRef = useRef<HTMLDivElement>(null);
 
-    const answerReceived = () => {
-        if (dotsRef.current) {
-            dotsRef.current.childNodes.forEach((child, index) => {
-                const c = child as HTMLDivElement;
+  const answerReceived = () => {
+    if (dotsRef.current) {
+      dotsRef.current.childNodes.forEach((child, index) => {
+        const c = child as HTMLDivElement;
 
-                // Setting check animations
-                if (c.classList.contains('dot')) {
-                    const dotAnimationDuration = ['.5s', '1.4s', '.5s'];
-                    const delay = index === 2 ? '.3s' : '0s';
-                    c.style.animation = `completeDot${index} ${dotAnimationDuration[index]} ${delay} forwards`;
-                } else {
-                    c.style.display = 'inline';
-                }
-            });
+        // Setting check animations
+        if (c.classList.contains("dot")) {
+          const dotAnimationDuration = [".5s", "1.4s", ".5s"];
+          const delay = index === 2 ? ".3s" : "0s";
+          c.style.animation = `completeDot${index} ${dotAnimationDuration[index]} ${delay} forwards`;
+        } else {
+          c.style.display = "inline";
         }
+      });
+    }
 
-        // Setting the correct display message and showing the redo-button
-        setTimeout(() => setGetAnswerText('Answer received!'), 1300);
-        setTimeout(() => setRedoAnswer(true), 1700);
-    };
+    // Setting the correct display message and showing the redo-button
+    setTimeout(() => setGetAnswerText("Answer received!"), 1300);
+    setTimeout(() => setRedoAnswer(true), 1700);
+  };
 
-    const playAnimation = useCallback(() => {
-        if (dotsRef.current) {
-            dotsRef.current.childNodes.forEach((child, index) => {
-                const c = child as HTMLDivElement;
+  const playAnimation = useCallback(() => {
+    if (dotsRef.current) {
+      dotsRef.current.childNodes.forEach((child, index) => {
+        const c = child as HTMLDivElement;
 
-                // Setting waiting animation on each dot and hiding the checkmark
-                if (c.classList.contains('dot')) {
-                    c.style.animation = `dotTyping 2s infinite ${index * 0.1}s`;
-                } else {
-                    c.style.display = 'none';
-                }
-            });
+        // Setting waiting animation on each dot and hiding the checkmark
+        if (c.classList.contains("dot")) {
+          c.style.animation = `dotTyping 2s infinite ${index * 0.1}s`;
+        } else {
+          c.style.display = "none";
         }
+      });
+    }
 
-        // Setting the correct display message and hiding the redo-button
-        setGetAnswerText('Waiting for an answer from your phone');
-        setRedoAnswer(false);
+    // Setting the correct display message and hiding the redo-button
+    setGetAnswerText("Waiting for an answer from your phone");
+    setRedoAnswer(false);
 
-        // Auto-runs the answerReceived after 5s - for testing
-        setTimeout(() => {
-            answerReceived();
-        }, 5000);
-    }, []);
+    // Auto-runs the answerReceived after 5s - for testing
+    setTimeout(() => {
+      answerReceived();
+    }, 5000);
+  }, []);
 
-    // useEffect for running the animation on page load
-    useEffect(() => {
-        if (currentStep === renderOnStep) {
-            playAnimation();
-        }
-    }, [currentStep, renderOnStep, playAnimation]);
+  // useEffect for running the animation on page load
+  useEffect(() => {
+    if (currentStep === renderOnStep) {
+      playAnimation();
+    }
+  }, [currentStep, renderOnStep, playAnimation]);
 
-    return (
-        <>
-            {currentStep !== renderOnStep ? null : (
-                <div className="w-4/5 h-screen relative">
-                    <BackButton
-                        currentStep={currentStep}
-                        onClick={() =>
-                            setTimeout(() => {
-                                goBackOneStep();
-                            }, 200)
-                        }
-                    />
+  return (
+    <>
+      {currentStep !== renderOnStep ? null : (
+        <div className="w-4/5 h-screen relative">
+          <BackButton
+            currentStep={currentStep}
+            onClick={() =>
+              setTimeout(() => {
+                goBackOneStep();
+              }, 200)
+            }
+          />
 
-                    <div className="absolute top-32">
-                        <div className="text-3xl leading-10 font-medium">{question}</div>
-                        <div className="font-normal text-gray-600 mt-2">{subText}</div>
-                        <div className="flex flex-col items-center mt-40">
-                            <div ref={dotsRef} className="mb-20 flex relative justify-between w-36">
-                                <div className="dot dot-one"></div>
-                                <div className="dot dot-two"></div>
-                                <div className="dot dot-three"></div>
-                                <div className="checkmark draw absolute hidden"></div>
-                            </div>
-                            <div className="text-lg">{getAnswerText}</div>
-                            {!redoAnswer ? null : (
-                                <button
-                                    onClick={() => playAnimation()}
-                                    className={`mt-14 px-4 py-1 text-lg place-items-center ${buttonStyle}`}
-                                >
-                                    Want a do-over?
-                                </button>
-                            )}
-                        </div>
-                    </div>
-                    <NextButton
-                        onClick={() =>
-                            answer
-                                ? setTimeout(() => {
-                                      handleChoice(question, answer);
-                                  }, 200)
-                                : null
-                        }
-                    />
-                </div>
-            )}
-        </>
-    );
+          <div className="absolute top-32">
+            <div className="text-3xl leading-10 font-medium">{question}</div>
+            <div className="font-normal text-gray-600 mt-2">{subText}</div>
+            <div className="flex flex-col items-center mt-40">
+              <div
+                ref={dotsRef}
+                className="mb-20 flex relative justify-between w-36"
+              >
+                <div className="dot dot-one"></div>
+                <div className="dot dot-two"></div>
+                <div className="dot dot-three"></div>
+                <div className="checkmark draw absolute hidden"></div>
+              </div>
+              <div className="text-lg">{getAnswerText}</div>
+              {!redoAnswer ? null : (
+                <button
+                  onClick={() => playAnimation()}
+                  className={`mt-14 px-4 py-1 text-lg place-items-center ${buttonStyle}`}
+                >
+                  Want a do-over?
+                </button>
+              )}
+            </div>
+          </div>
+          <NextButton
+            currentStep={currentStep}
+            onClick={() =>
+              answer
+                ? setTimeout(() => {
+                    handleChoice(question, answer);
+                  }, 200)
+                : null
+            }
+          />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default TextQuestion;
 
 const buttonStyle =
-    'shadow-inactive focus:shadow-focused focus:border-blue-500 border-4 border-transparent focus:outline-none rounded-xl active:scale-90 animate transition transform';
+  "shadow-inactive focus:shadow-focused focus:border-blue-500 border-4 border-transparent focus:outline-none rounded-xl active:scale-90 animate transition transform";
