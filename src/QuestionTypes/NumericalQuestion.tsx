7d23bd39126c281f63e6debf8ed8514d03577478
Anders H <ahha15@student.aau.dk>
2021-03-04 13:57:03 +0100

    Missing touchless on: TextQuestion and SliderQuestion

--- src/QuestionTypes/NumericalQuestion.tsx
+++ src/QuestionTypes/NumericalQuestion.tsx
@@ -1,5 +1,6 @@
 import { Backspace } from "phosphor-react";
 import React, { useState } from "react";
+import { Touchless } from "touchless-navigation";
 import BackButton from "../BackButton";
 import NextButton from "../NextButton";
 
@@ -33,75 +34,95 @@ const NumericalQuestion: React.SFC<{
               <span className="animate-pulse font-light">|</span>
             </div>
             <div className="grid-cols-3  grid w-64 gap-3 mt-6 justify-items-center mx-auto text-2xl font-medium">
-              <div
-                onClick={() => setAmount(amount + "1")}
-                className={`${buttonStyle} px-6 py-3`}
-              >
-                1
-              </div>
-              <div
-                onClick={() => setAmount(amount + "2")}
-                className={`${buttonStyle} px-6 py-3`}
-              >
-                2
-              </div>
+              <Touchless>
+                <div
+                  onClick={() => setAmount(amount + "1")}
+                  className={`${buttonStyle} px-6 py-3`}
+                >
+                  1
+                </div>
+              </Touchless>
+              <Touchless>
+                <div
+                  onClick={() => setAmount(amount + "2")}
+                  className={`${buttonStyle} px-6 py-3`}
+                >
+                  2
+                </div>
+              </Touchless>
               <div
                 onClick={() => setAmount(amount + "3")}
                 className={`${buttonStyle} px-6 py-3`}
               >
                 3
               </div>
-              <div
-                onClick={() => setAmount(amount + "4")}
-                className={`${buttonStyle} px-6 py-3`}
-              >
-                4
-              </div>
-              <div
-                onClick={() => setAmount(amount + "5")}
-                className={`${buttonStyle} px-6 py-3`}
-              >
-                5
-              </div>
-              <div
-                onClick={() => setAmount(amount + "6")}
-                className={`${buttonStyle} px-6 py-3`}
-              >
-                6
-              </div>
-              <div
-                onClick={() => setAmount(amount + "7")}
-                className={`${buttonStyle} px-6 py-3`}
-              >
-                7
-              </div>
-              <div
-                onClick={() => setAmount(amount + "8")}
-                className={`${buttonStyle} px-6 py-3`}
-              >
-                8
-              </div>
-              <div
-                onClick={() => setAmount(amount + "9")}
-                className={`${buttonStyle} px-6 py-3`}
-              >
-                9
-              </div>
-              <div
-                onClick={() => setAmount(amount + "0")}
-                className={`${buttonStyle} px-6 py-3`}
-              >
-                0
-              </div>
-              <div
-                onClick={() =>
-                  setAmount(amount.substring(0, amount.length - 1))
-                }
-                className={`${buttonStyle} px-6 py-3 col-span-2 flex items-center font-normal text-2xl`}
-              >
-                <Backspace className="mr-2" />
-                Delete
-              </div>
+              <Touchless>
+                <div
+                  onClick={() => setAmount(amount + "4")}
+                  className={`${buttonStyle} px-6 py-3`}
+                >
+                  4
+                </div>
+              </Touchless>
+              <Touchless>
+                <div
+                  onClick={() => setAmount(amount + "5")}
+                  className={`${buttonStyle} px-6 py-3`}
+                >
+                  5
+                </div>
+              </Touchless>
+              <Touchless>
+                <div
+                  onClick={() => setAmount(amount + "6")}
+                  className={`${buttonStyle} px-6 py-3`}
+                >
+                  6
+                </div>
+              </Touchless>
+              <Touchless>
+                <div
+                  onClick={() => setAmount(amount + "7")}
+                  className={`${buttonStyle} px-6 py-3`}
+                >
+                  7
+                </div>
+              </Touchless>
+              <Touchless>
+                <div
+                  onClick={() => setAmount(amount + "8")}
+                  className={`${buttonStyle} px-6 py-3`}
+                >
+                  8
+                </div>
+              </Touchless>
+              <Touchless>
+                <div
+                  onClick={() => setAmount(amount + "9")}
+                  className={`${buttonStyle} px-6 py-3`}
+                >
+                  9
+                </div>
+              </Touchless>
+              <Touchless>
+                <div
+                  onClick={() => setAmount(amount + "0")}
+                  className={`${buttonStyle} px-6 py-3`}
+                >
+                  0
+                </div>
+              </Touchless>
+              <Touchless>
+                <div
+                  onClick={() =>
+                    setAmount(amount.substring(0, amount.length - 1))
+                  }
+                  className={`${buttonStyle} px-6 py-3 col-span-2 flex items-center font-normal text-2xl`}
+                >
+                  <Backspace className="mr-2" />
+                  Delete
+                </div>
+              </Touchless>
             </div>
           </div>
           <NextButton
--- src/QuestionTypes/SingleChoiceListQuestion.tsx
+++ src/QuestionTypes/SingleChoiceListQuestion.tsx
@@ -1,65 +1,68 @@
-import React from 'react';
-import BackButton from '../BackButton';
+import React from "react";
+import { Touchless } from "touchless-navigation";
+import BackButton from "../BackButton";
 
 export interface SingleChoiceListQuestionProps {
-    currentStep: number;
-    renderOnStep: number;
-    question: string;
-    answersArray: string[];
-    handleChoice: (question: string, answer: string) => void;
-    goBackOneStep: () => void;
+  currentStep: number;
+  renderOnStep: number;
+  question: string;
+  answersArray: string[];
+  handleChoice: (question: string, answer: string) => void;
+  goBackOneStep: () => void;
 }
 
 const SingleChoiceListQuestion: React.SFC<SingleChoiceListQuestionProps> = ({
-    currentStep,
-    renderOnStep,
-    question,
-    answersArray,
-    handleChoice,
-    goBackOneStep,
+  currentStep,
+  renderOnStep,
+  question,
+  answersArray,
+  handleChoice,
+  goBackOneStep,
 }) => {
-    //   console.log(answersArray);
+  //   console.log(answersArray);
 
-    return (
-        <>
-            {currentStep !== renderOnStep ? null : (
-                <div className="w-4/5 relative h-screen">
-                    <BackButton
-                        currentStep={currentStep}
-                        onClick={() =>
-                            setTimeout(() => {
-                                goBackOneStep();
-                            }, 200)
-                        }
-                    />
-                    <div className="absolute top-32">
-                        <div className="text-3xl leading-10 font-bold">{question}</div>
-                        <div className="mt-2 text-gray-600">Choose one.</div>
-                        <ul className="w-4/5 mt-10 text-lg font-normal">
-                            {answersArray.map((answer) => {
-                                return (
-                                    <li
-                                        key={answer}
-                                        onClick={() =>
-                                            setTimeout(() => {
-                                                handleChoice(question, answer);
-                                            }, 200)
-                                        }
-                                        className={`${buttonStyle} pl-4 py-3 mb-5`}
-                                    >
-                                        {answer}
-                                    </li>
-                                );
-                            })}
-                        </ul>
-                    </div>
-                </div>
-            )}
-        </>
-    );
+  return (
+    <>
+      {currentStep !== renderOnStep ? null : (
+        <div className="w-4/5 relative h-screen">
+          <BackButton
+            currentStep={currentStep}
+            onClick={() =>
+              setTimeout(() => {
+                goBackOneStep();
+              }, 200)
+            }
+          />
+          <div className="absolute top-32">
+            <div className="text-3xl leading-10 font-bold">{question}</div>
+            <div className="mt-2 text-gray-600">Choose one.</div>
+            <ul className="w-4/5 mt-10 text-lg font-normal">
+              {answersArray.map((answer) => {
+                return (
+                  <Touchless>
+                    <li
+                      key={answer}
+                      onClick={() =>
+                        setTimeout(() => {
+                          handleChoice(question, answer);
+                        }, 200)
+                      }
+                      className={`${buttonStyle} pl-4 py-3 mb-5`}
+                    >
+                      {answer}
+                    </li>
+                  </Touchless>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default SingleChoiceListQuestion;
 
 const buttonStyle =
-    'shadow-inactive focus:shadow-focused focus:border-blue-500 border-4 border-transparent focus:outline-none rounded-xl active:scale-90 animate transition transform';
+  "shadow-inactive focus:shadow-focused focus:border-blue-500 border-4 border-transparent focus:outline-none rounded-xl active:scale-90 animate transition transform";
