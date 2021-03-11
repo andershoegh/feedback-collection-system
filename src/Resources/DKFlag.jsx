76ce3896afb60509e5b5826604931d4348575b8f
Anders H <ahha15@student.aau.dk>
2021-03-11 09:53:56 +0100

    added flags

---
+++ src/Resources/DKFlag.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function DKFlag() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 370 280">
+      2<path fill="#c60c30" d="M0 0H370V280H0z"></path>3
+      <path fill="#fff" d="M120 0H160V280H120z"></path>4
+      <path fill="#fff" d="M0 120H370V160H0z"></path>5
+    </svg>
+  );
+}
+
+export default DKFlag;
